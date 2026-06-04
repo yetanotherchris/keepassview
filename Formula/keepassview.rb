cf04cc1d560fada0f6340af66e9d981f247a6b43
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-06-04 18:09:10 +0000

    chore: update package manifests to v1.0.0

--- Formula/keepassview.rb
+++ Formula/keepassview.rb
@@ -1,25 +1,25 @@
 class Keepassview < Formula
   desc "Read-only KeePass (.kdbx) database viewer that runs a local web server"
   homepage "https://github.com/yetanotherchris/keepassview"
-  version "VERSION"
+  1.0.0 "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/keepassview/releases/download/vVERSION/keepassview-darwin-arm64.tar.gz"
-      sha256 "SHA256"
+      url "https://github.com/yetanotherchris/keepassview/releases/download/v1.0.0/keepassview-darwin-arm64.tar.gz"
+      sha256 "ae08c234f146022975bce7befae570436cc7d03abff4200568663fddcdc53049"
     else
-      url "https://github.com/yetanotherchris/keepassview/releases/download/vVERSION/keepassview-darwin-amd64.tar.gz"
-      sha256 "SHA256"
+      url "https://github.com/yetanotherchris/keepassview/releases/download/v1.0.0/keepassview-darwin-amd64.tar.gz"
+      sha256 "3da899e699578e2c9348a19527b399e1b1f6c7b9e50183b36b7acf36f080233c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/keepassview/releases/download/vVERSION/keepassview-linux-arm64.tar.gz"
-      sha256 "SHA256"
+      url "https://github.com/yetanotherchris/keepassview/releases/download/v1.0.0/keepassview-linux-arm64.tar.gz"
+      sha256 "41c3059863336689d65a9dbe00717e03dbb21f8ad1192e8e1204a578c4a3cbe0"
     else
-      url "https://github.com/yetanotherchris/keepassview/releases/download/vVERSION/keepassview-linux-amd64.tar.gz"
-      sha256 "SHA256"
+      url "https://github.com/yetanotherchris/keepassview/releases/download/v1.0.0/keepassview-linux-amd64.tar.gz"
+      sha256 "5e9d78272aaffa0f476d46170fbf9c75d9357a82c8dfcec7d88ad9daf076602b"
     end
   end
 
@@ -31,6 +31,6 @@ class Keepassview < Formula
   end
 
   test do
-    assert_match "keepassview version", shell_output("#{bin}/keepassview --version")
+    assert_match "keepassview 1.0.0", shell_output("#{bin}/keepassview --1.0.0")
   end
 end
